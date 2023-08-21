17905fee81032737edc318a472f1b631caaaca32
Tuan Nguyen <tuan.nat915@gmail.com>
2023-08-20 22:52:17 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const songs = [
+    {
+        name: "Color_Out",
+        singer: "Alone",
+        path: "./assets/music/Color_Out.mp3",
+        image: "./assets/img/Alone_-_Color_Out.jpeg",
+    },
+    {
+        name: "You'll_Be_Fine_ft._Crushboys_(Original_Mix)_-_erickfill",
+        singer: "Erick_Fill_&amp;_Alwaro_",
+        path: "./assets/music/Erick_Fill_&amp;_Alwaro_-_You'll_Be_Fine_ft._Crushboys_(Original_Mix)_-_erickfill.mp3.mp3",
+        image: "./assets/img/Erick_Fill_&amp;_Alwaro_-_You'll_Be_Fine_ft._Crushboys_(Original_Mix)_-_erickfill.jpeg",
+    },
+    {
+        name: "Crystals_In_Our_Eyes",
+        singer: "Romancoke",
+        path: "./assets/music/Romancoke_-_Crystals_In_Our_Eyes.mp3",
+        image: "./assets/img/Romancoke_-_Crystals_In_Our_Eyes.jpeg",
+    },
+    {
+        name: "Sau-Nay-Chung-Ta-Giau",
+        singer: "Khac Viet",
+        path: "./assets/music/Sau-Nay-Chung-Ta-Giau-Khac-Viet-ACV.mp3",
+        image: "./assets/img/Sau-Nay-Chung-Ta-Giau-Khac-Viet-ACV.jpeg",
+    },
+];
